21a9d2eb9a5c1772b42cba78ccb18cf349b8ffa8
BhagyPatel <patelbhagy1810@gmail.com>
2025-03-15 13:50:14 +0530

    console msg deleted

--- controllers/listings.js
+++ controllers/listings.js
@@ -39,7 +39,6 @@ module.exports.createListing = async (req, res, next) => {
     newListing.image = {url, filename};
     newListing.geometry = response.body.features[0].geometry;
     let savedListing = await newListing.save();
-    console.log(savedListing);
     req.flash("success", "New Listing Created Succesfully!!");
     res.redirect("/listings");
 };
